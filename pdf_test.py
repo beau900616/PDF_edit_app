c1463ba1426c0bc3f720892cde26355566e3a1e1
Beau900516 <beau_wang@kimo.com>
2025-07-12 12:16:00 +0800

    pdf_1.1

---
+++ pdf_test.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+import os, uuid
+
+app = Flask(__name__)
+app.secret_key = 'super_secret_key'
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def index():
+    return render_template('pdf_manu.html')
+
+@app.route('/upload/<target>', methods=['POST'])
+def upload(target):
+    uploaded_file = request.files['pdf_file']
+    if uploaded_file.filename != '':
+        original_name = uploaded_file.filename
+        ext = os.path.splitext(original_name)[1]
+        filename = f"{uuid.uuid4().hex}{ext}"
+        uploaded_file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+        # Save to session separately by target
+        session[f'{target}_original_name'] = original_name
+        session[f'{target}_filename'] = filename
+
+    return redirect(url_for('index'))
+
+@app.route('/uploads/<path:filename>')
+def serve_pdf(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
